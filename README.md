5190419def36907648807c6ae72891121c01bb1c
Maximilian Richt <maxi@richt.name>
2020-03-23 03:03:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+bbb-easy-join
+-------------
+
+An easy frontend to BigBlueButton without signups - everybody can just entry an meeting room name, the first user gets moderator level access.
+
+HTML template, css, images are from the [bigbluebutton default pages](https://github.com/bigbluebutton/bigbluebutton/tree/master/bigbluebutton-config/web).
+
+BigBlueButton and the BigBlueButton Logo are trademarks of [BigBlueButton Inc](http://bigbluebutton.org).
+
---
+++ app.js
@@ -0,0 +1,110 @@
+require('dotenv').config()
+const crypto = require('crypto')
+const fetch = require('node-fetch')
+const xmlparser = require('fast-xml-parser')
+const querystring = require('querystring')
+const slugify = require('slugify')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const host = process.env.HOST || '0.0.0.0'
+
+const BBB_API_URL = process.env.BBB_API_URL
+const BBB_API_SECRET = process.env.BBB_API_SECRET
+
+const sha1 = function (data) {
+  return crypto.createHash("sha1").update(data, "binary").digest("hex")
+}
+
+const checksum = function (method, query) {
+  return sha1(method + query + BBB_API_SECRET)
+}
+
+const buildApiUrl = function (method, params) {
+  var url = BBB_API_URL + (BBB_API_URL.charAt(BBB_API_URL.length - 1) === '/' ? '' : '/') + 'api/' + method
+  var query = querystring.stringify(params)
+  var cs = checksum(method, query)
+  params['checksum'] = cs
+  return url + '?' + querystring.stringify(params)
+}
+
+const callApi = async function (method, params) {
+  return fetch(buildApiUrl(method, params)).then(res => res.text()).then(data => xmlparser.parse(data).response)
+}
+
+const createMeeting = async function (name, id) {
+  var params = { meetingID: id, name: name }
+  return await callApi('create', params)
+}
+
+const getMeetingInfo = async function (id) {
+  var params = { meetingID: id }
+  return await callApi('getMeetingInfo', params)
+}
+
+const joinMeeting = async function (id, name, password) {
+  var params = { meetingID: id, fullName: name, password: password, redirect: false };
+  return await callApi('join', params);
+}
+
+const joinMeetingUrl = function (id, name, password) {
+  var params = { meetingID: id, fullName: name, password: password, redirect: true };
+  return buildApiUrl('join', params);
+}
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded())
+
+app.get('/', function(req, res) {
+  res.redirect('/b');
+})
+
+app.get('/b', function (req, res) {
+  res.render('index')
+})
+
+app.post('/b', async function (req, res) {
+  var roomName = req.body.room
+  var room = slugify(roomName)
+
+  var meet = await createMeeting(roomName, room)
+  if (meet.returncode === 'FAILED' && meet.messageKey !== 'idNotUnique') {
+    res.redirect('/b')
+    return
+  }
+
+  res.redirect('/b/' + room)
+})
+
+app.get('/b/:room', async function (req, res){
+  var room = slugify(req.params.room)
+
+  var info = await getMeetingInfo(room)
+  if (info.returncode === 'FAILED') {
+    res.redirect('/b')
+    return
+  }
+
+  res.render('join', { room: room, info: info })
+})
+
+app.post('/b/:room', async function (req, res) {
+  var room = slugify(req.params.room)
+  var name = req.body.name
+  var info = await getMeetingInfo(room)
+  if (info.returncode === 'FAILED') {
+    res.redirect('/b')
+    return
+  }
+
+  var password = info.attendeePW
+  if (info.hasUserJoined !== "false" || info.participantCount == 0) {
+    password = info.moderatorPW
+  }
+
+  res.redirect(joinMeetingUrl(room, name, password))
+})
+
+app.listen(port, host, () => console.log(`Running on ${host}:${port}`))
+
