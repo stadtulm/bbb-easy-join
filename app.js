dddec181b9fd4d6bf6de01ef27c960f28d6af15f
Maximilian Richt <maxi@richt.name>
2020-03-23 11:53:24 +0000

    add default option to express.urlencoded to fix deprecation notice

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const joinMeetingUrl = function (id, name, password) {
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function(req, res) {
   res.redirect('/b');
